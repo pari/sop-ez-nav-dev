2987ee87319ac79647487bd4fe6edef9f23b2adc
Pari Nannapaneni <paripurnachand@gmail.com>
2020-08-20 23:38:59 +0530

    save point

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 // Initialize the application
 
 	EcwidApp.init({
-	  app_id: "sample-native-app", // use your application namespace
+	  app_id: "sop-ez-nav-dev", // use your application namespace
 	  autoloadedflag: true, 
 	  autoheight: true
 	});
